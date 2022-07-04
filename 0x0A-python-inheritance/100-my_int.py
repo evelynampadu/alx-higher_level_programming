f64f71ae56ff457306be20672fd84f57c30dd7ef
Evelyn <81477740+evelynampadu@users.noreply.github.com>
2022-07-04 22:21:16 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,40 +1,14 @@
 #!/usr/bin/python3
-"""
-=============================
-Module with a class Myint
-=============================
-"""
+"""This program create a new int called MyInt and is rebel!!!"""
 
 
 class MyInt(int):
-    """class that inherits from int"""
+    """Class MyInt is like an int but Rebel"""
 
-    def __init__(self, my_int):
-        """for initialize a value my_int"""
+    def __eq__(self, other_num):
+        """Rebel!!! equal is not equal"""
+        return super().__ne__(other_num)
 
-        self.my_int = my_int
-
-    @property
-    def my_int(self):
-        return self.__my_int
-
-    @my_int.setter
-    def my_int(self, my_int):
-        if type(my_int) is not int:
-            raise TypeError("my_int must be an integer")
-        else:
-            self.__my_int = my_int
-
-    def __eq__(self, other):
-        """equal method"""
-
-        if self.my_int == other:
-            return False
-        else:
-            return True
-
-    def __ne__(self, other):
-            if self.my_int != other:
-                return False
-            else:
-                return True
+    def __ne__(self, other_num):
+        """Rebel!!! not equal is equal"""
+        return super().__eq__(other_num)
