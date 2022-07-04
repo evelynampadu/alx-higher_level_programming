01c7c3ef19862c0869db2e485be1183c2fc0f26d
Evelyn <81477740+evelynampadu@users.noreply.github.com>
2022-07-04 22:23:34 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
+"""
+This program create a Square from a Rectangle
+"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
-
+    """Class Squared based from Rectangle"""
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
-        super().__init__(size, size)
+        """Constructor of Square"""
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
-        """rectangle area"""
-
+        """Calculate the area of a Square"""
         return self.__size ** 2
